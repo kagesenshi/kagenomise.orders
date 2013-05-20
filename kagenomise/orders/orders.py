e2b31457c4d814706b43ee75c708b4c059712c31
Izhar Firdaus <izhar@inigo-tech.com>
2013-05-21 00:02:18 +0800

    trigger all necessary events to make object properly working

--- kagenomise/orders/orders.py
+++ kagenomise/orders/orders.py
@@ -12,6 +12,11 @@ from z3c.relationfield import RelationValue
 from zope.component import getUtility
 import datetime
 from Products.CMFCore.interfaces import IContentish
+from zope.event import notify
+from zope.lifecycleevent import (
+        ObjectCreatedEvent, ObjectModifiedEvent,
+        ObjectAddedEvent, ObjectMovedEvent
+)
 
 class Orders(grok.Adapter):
     grok.implements(IOrderManager)
@@ -23,6 +28,9 @@ class Orders(grok.Adapter):
     def repository(self):
         if 'cart_orders' not in self.context.keys():
             _createObjectByType('Folder', self.context, 'cart_orders')
+            orders = self.context._getOb('cart_orders')
+            notify(ObjectCreatedEvent(orders))
+            notify(ObjectAddedEvent(orders))
         return self.context._getOb('cart_orders')
 
     def new_from_cart(self, data):
@@ -32,7 +40,8 @@ class Orders(grok.Adapter):
         tempid = str(time.time())
         order = _createObjectByType('kagenomise.orders.order',
                     orders, tempid)
-
+        notify(ObjectCreatedEvent(order))
+        notify(ObjectAddedEvent(order))
         now = datetime.datetime.today()
         title = u'%s : %s' % (now.strftime('%Y%m%d'), data['recipient_name'])
     
@@ -44,6 +53,8 @@ class Orders(grok.Adapter):
         orders._delObject(tempid, suppress_events=True)
         orders._setObject(oid, order, set_owner=0, suppress_events=True)
         
+        notify(ObjectMovedEvent(order, oldParent=orders, oldName=tempid, 
+            newParent=orders, newName=oid))
         order.setTitle(title)
 
         for k in ['recipient_name','shipping_address','recipient_email',
@@ -51,13 +62,21 @@ class Orders(grok.Adapter):
             setattr(order, k, data[k])
         order.reindexObject()
 
-        self._add_items(order, data['items'])
+        entries = self._add_items(order, data['items'])
+
+        notify(ObjectModifiedEvent(order))
+        for entry in entries:
+            notify(ObjectModifiedEvent(entry))
 
         return order
 
     def _add_items(self, order, items):
+        result = []
         for item in items:
-            self._add_item(order, item)
+            obj = self._add_item(order, item)
+            if obj:
+                result.append(obj)
+        return result
 
     def _add_item(self, order, item):
 
@@ -73,6 +92,8 @@ class Orders(grok.Adapter):
         tempid = str(time.time())
         obj = _createObjectByType('kagenomise.orders.orderentry',
                 order, tempid)
+        notify(ObjectCreatedEvent(obj))
+        notify(ObjectAddedEvent(obj))
         transaction.savepoint(optimistic=True)
         oid = INameChooser(order).chooseName(title, obj)
 
@@ -80,7 +101,8 @@ class Orders(grok.Adapter):
         obj._setId(oid)
         order._delObject(tempid, suppress_events=True)
         order._setObject(oid, obj, set_owner=0, suppress_events=True)
-
+        notify(ObjectMovedEvent(obj, oldParent=order, oldName=tempid,
+            newParent=order, newName=oid))
         if item_ref:
             obj.unit_price = item_ref.price
         else:
